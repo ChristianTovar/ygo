4b97fe276793dd1dc6b4bf24bb2cfe11942c0046
ChristianTovar <tovarchristian21@gmail.com>
2020-07-09 19:37:08 -0500

    Add github CI

--- config/config.exs
+++ config/config.exs
@@ -16,7 +16,7 @@ if Mix.env() != :prod do
         tasks: [
           {:cmd, "mix doctor"},
           {:cmd, "mix test"},
-          {:cmd, "echo 'Freaking Success!'"}
+          {:cmd, "echo 'Freaking Success! 😎👍'"}
         ]
       ]
     ]
